9d0509b8f2afe4bf9234c9af01eb056c94c6a80b
Code Wizard <132194411+deivanayakis@users.noreply.github.com>
2024-02-23 01:14:12 +0530

    Delete Extension directory

--- Extension/ChromeExtension/content.js
+++
@@ -1,8 +0,0 @@
-// content.js
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action === 'getURL') {
-      console.log('Test message received in content script!');
-      sendResponse({ url: window.location.href });
-    }
-  });
-  
